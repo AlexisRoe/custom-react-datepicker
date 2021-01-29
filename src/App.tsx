ddf5368d2fbd5192a48d70855db4403b7f849574
Alexis Roehrling <A.Roehrling@gmx.de>
2021-01-29 17:30:54 +0100

    Add basic functionallity

--- src/App.tsx
+++ src/App.tsx
@@ -4,28 +4,41 @@ import { whatIsYourDate, createTableHeader, createTableBody } from "./Dates/"
 import today from "./icons/today.png"
 
 function App() {
-  const [show, setShow] = useState(false);
+  const date = new Date();
+  const options = { year: 'numeric', month: 'long', day: 'numeric' }
 
-  const info = whatIsYourDate('en-EN', new Date());
+  const [show, setShow] = useState(false);
+  const [htmlValue, setHtmlValue] = useState("")
+  const [customValue, setCustomValue] = useState(() => new Intl.DateTimeFormat('de-DE', options).format(date))
+
+  const info = whatIsYourDate('en-EN', date);
   const Calendar = createTableBody(info.start.weekday, info.end.day);
 
   const showSelectBox = () => {
     setShow(!show)
   };
 
+  const customSelect = (day: string) => {
+    if (day === null) {
+      return
+    }
+    setCustomValue(`${day} ${info.today.month} ${info.today.year}`);
+    showSelectBox();
+  }
+
   return (
     <>
       <div className="card-container flex border">
         <label htmlFor="html-datepicker" className="label">HTML Date Picker</label>
-        <input type="date" id="html-datepicker" />
+        <input type="date" id="html-datepicker" value={htmlValue} onChange={event => setHtmlValue(event.target.value)} />
         <fieldset className="output border">
           <legend className="legend">Output</legend>
-          ...
+          {htmlValue}
         </fieldset>
       </div>
       <div className="card-container flex border">
         <label className="label">Custom Date Picker</label>
-        <div className="datepicker border">10.02.2021
+        <div className="datepicker border">{customValue}
           <button className="datepicker--button datepicker--button-choose" onClick={showSelectBox}>
             <img src={today} alt="select data" />
           </button>
@@ -45,7 +58,7 @@ function App() {
                     <tr key={Object.keys(Calendar)[index]}>
                       {row.map((day: any, index: number) => {
                         return (
-                          <td key={index}>{day}</td>
+                          <td key={index} onClick={() => customSelect(day)}>{day}</td>
                         )
                       })}
                     </tr>
@@ -57,7 +70,7 @@ function App() {
         </div>
         <fieldset className="output border">
           <legend className="legend">Output</legend>
-          ...
+          {customValue}
         </fieldset>
       </div>
     </>
